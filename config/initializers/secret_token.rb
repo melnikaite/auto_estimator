fcb4875b17da413896f5046c699fa328ce739c1a
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2012-12-26 20:08:05 +0300

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AutoEstimator::Application.config.secret_token = '800110e55116f6a3f5154ce20cce58d0f731e1908b7c94b0f2fe8e3b602e2bb55ac1973b459e25e91e7399848f2f3267879a1da8c113d72686f8e5beb52c669d'
