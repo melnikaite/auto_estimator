4c1cae73c9dbddc297f4b5871ef205e59360f878
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2012-12-27 19:10:38 +0300

    projects controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,44 @@
+class ProjectsController < ApplicationController
+  before_filter :find_project, :only => [:show, :edit, :update, :destroy]
+
+  def index
+    @projects = Project.paginate(:page => params[:page])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.create(params[:project])
+    if @project.persisted?
+      flash[:notice] = "Project created"
+      redirect_to root_path
+    else
+      flash[:error] = @project.errors.full_messages.join("<br />").html_safe
+      render :new
+    end
+  end
+
+  def update
+    if @project.update_attributes(params[:project])
+      flash[:notice] = "Project updated"
+      redirect_to project_path(@project)
+    else
+      flash[:error] = @project.errors.full_messages.join("<br />").html_safe
+      render :edit
+    end
+  end
+
+  def destroy
+    @project.destroy
+    flash[:notice] = "Project deleted"
+    redirect_to root_path
+  end
+
+  private
+
+  def find_project
+    @project = Project.find_by_id(params[:id]) || not_found
+  end
+end
